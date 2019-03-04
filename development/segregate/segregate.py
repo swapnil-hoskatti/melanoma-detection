acb857d3855ee82d9cf6860a4df9c8980c0e6a44
Gavin Dsouza <gavin18d@gmail.com>
2019-03-04 14:45:12 +0530

    [CHANGES] segreg changes to use os.path.join instaed of str formatting

--- development/segregate/segregate.py
+++ development/segregate/segregate.py
@@ -5,31 +5,34 @@ Created on Thu Sep 27 18:25:09 2018
 @author: Gavin
 """
 import os
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pickle
 
 def segreg():
     #img_list = os.listdir(r".\all")
     
     csv_file = open('godfather.csv').read().split('\n')
-    
+    valid_img = [x for x in os.listdir('all') if x[-3:] == 'jpg']
+
     for line in csv_file[1:-1]:
         img_name, MEL, NV, BCC, AKIEC, BKL, DF, VASC = line.split(',')
         img_name = img_name + '.jpg'
-        if MEL == "1.0":
-            os.rename(r".\all\{}".format(img_name), r".\all\MEL\{}".format(img_name))
-        elif NV == "1.0":
-            os.rename(r".\all\{}".format(img_name), r".\all\NV\{}".format(img_name))    
-        elif BCC == "1.0":
-            os.rename(r".\all\{}".format(img_name), r".\all\BCC\{}".format(img_name)) 
-        elif AKIEC == "1.0":
-            os.rename(r".\all\{}".format(img_name), r".\all\AKIEC\{}".format(img_name))
-        elif BKL == "1.0":
-            os.rename(r".\all\{}".format(img_name), r".\all\BKL\{}".format(img_name))
-        elif DF == "1.0":
-            os.rename(r".\all\{}".format(img_name), r".\all\DF\{}".format(img_name))
-        elif VASC == "1.0":
-            os.rename(r".\all\{}".format(img_name), r".\all\VASC\{}".format(img_name))
+
+        if img_name in valid_img:
+            if MEL == "1.0":
+                os.rename(os.path.join("all", img_name), os.path.join("all", "MEL", img_name))
+            elif NV == "1.0":
+                os.rename(os.path.join("all", img_name), os.path.join("all", "NV", img_name))    
+            elif BCC == "1.0":
+                os.rename(os.path.join("all", img_name), os.path.join("all", "BCC", img_name))
+            elif AKIEC == "1.0":
+                os.rename(os.path.join("all", img_name), os.path.join("all", "AKIEC", img_name))
+            elif BKL == "1.0":
+                os.rename(os.path.join("all", img_name), os.path.join("all", "BKL", img_name))
+            elif DF == "1.0":
+                os.rename(os.path.join("all", img_name), os.path.join("all", "DF", img_name))
+            elif VASC == "1.0":
+                os.rename(os.path.join("all", img_name), os.path.join("all", "VASC", img_name))
             
 def pickleem():
     folders = [x for x in os.listdir(r".\all") if f"{x}.pickle" not in os.listdir()]
@@ -39,4 +42,4 @@ def pickleem():
         print(f"{folder} done")
         
     
-pickleem()
+segreg()
